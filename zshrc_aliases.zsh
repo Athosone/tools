00bd83b09f6924f989ade66f9fc93c6a44e06839
Ayrton <awerck@nuglif.com>
2018-08-17 15:50:39 -0400

    initial commit

---
+++ zshrc_aliases.zsh
@@ -0,0 +1,13 @@
+alias ta="tig --all"
+alias clean="rm -f *~"
+alias clean-xcode="rm -rf /Volumes/Users_Data/Users/awerck/Library/Developer/Xcode/DerivedData" 
+alias replica="cd ~/Dev/ios_replica"
+alias ows="find . -maxdepth 1 -name '*.xcworkspace' | head -n 1 | xargs open"
+alias gawip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --amend --no-edit --no-verify -m "--wip-- [skip ci]"'
+alias e="vim"
+alias br="branch"
+alias co='function __gitCheckout() { git checkout "$@" }; __gitCheckout'
+alias gc-="git checkout -"
+alias edit-aliases="vim ~/.oh-my-zsh/custom/zshrc_aliases.zsh"
+alias find-file='function __findFile() { find . -name "$1" -type f | xargs ls -lh }; __findFile'
+alias source-sh="source ~/.zshrc"
